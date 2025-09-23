220c31bbc9006d7ad85f5f68434110d8dcaad0ad
Krutika Gadge <krutikargadge29@gmail.com>
2025-09-23 09:56:04 +0530

    Add files via upload

---
+++ Practical 1/Practical_1_vector.R
@@ -0,0 +1,60 @@
+x=10:20
+print(x)
+
+y=x+2
+print(y)
+
+z=y*3
+print(z)
+
+answer=(z-6)/3
+print(answer)
+
+one=(((x+2)*3)-6)/3
+print(one)
+
+one=(((10:20 +2)*3)-6)/3
+print(one)
+
+c=seq(1,12,0.5)
+print(c)
+
+m=seq(1,10)^3
+print(m)
+
+
+vec <- c(1, 2, 3, 2, 4, 2, 5, 2, 6)
+x <- 2
+count <- sum(vec == x)
+print(count)
+
+v1 <- c(1, 2, 3, 4, 5)
+v2 <- c(3, 4, 5, 6, 7)
+common <- intersect(v1,v2)
+print(common)
+
+sequence_vector <- c(1:20, 19:1)
+print(sequence_vector)
+
+quotes <- c("Data is the new oil", 
+            "Big data means big responsibility", 
+            "Clean data is gold")
+matches <- grepl("data", quotes, ignore.case = TRUE)
+print(matches)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
